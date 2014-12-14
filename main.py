98b7f590fba48322ea3ed3ba2014135bb41773fa
mcoenca <m.coenca@gmail.com>
2014-12-13 22:37:47 -0800

    connecting to the default meteor local database on por t 3001 in the python script

--- main.py
+++ main.py
@@ -45,8 +45,8 @@ def words_from_component(component_matrix, word_matrix):
 def insert_res_in_mongodb(res, params, n_components):
     "Insert the result of our spca in the db so it can be stored, retrieved and displayed in a web interface later"
     # Connects to Mongo DB and use the collection "components"
-    connection = pymongo.Connection()
-    database = connection["sviepbd"]
+    connection = pymongo.Connection('127.0.0.1', 3001)
+    database = connection["local"]
     components = database.components
 
     """
