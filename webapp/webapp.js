27ba7e7b0bc6bf81e2be12ffe564ca8c08b27649
mcoenca <m.coenca@gmail.com>
2014-12-15 19:41:41 +0100

    passing arguments to the meteor method

--- webapp/webapp.js
+++ webapp/webapp.js
@@ -29,18 +29,35 @@ if (Meteor.isServer) {
         }
         if (stderr) console.log(stderr);
     };
+  var dictToString = function (args_dict) {
+    res = "";
+    res = res + args_dict.matrix_path + " " + 
+    args_dict.n_lines + " " +
+    args_dict.n_col + " " +
+    args_dict.word_path + " " +
+    args_dict.k + " " +
+    args_dict.h + " " +
+    args_dict.n_components + " " +
+    args_dict.norm_row + " " +
+    args_dict.precision;
+    console.log(res);
+    return res;
+  };
 
-  var executePythonSPCA = function (args, callback) {
-    Meteor.npmRequire('child_process').exec("python " + "/Users/mcoenca/Documents/Thnktwice/Code/sviep-bigdata-python/call_spca.py " + "/Users/mcoenca/Documents/Thnktwice/Code/data/many-results_matrix.csv 2950 9000 /Users/mcoenca/Documents/Thnktwice/Code/data/many-results_words.csv 10 8000 2 True 1.0e-8", callback);
+  var executePythonSPCA = function (args_string, callback) {
+    Meteor.npmRequire('child_process').exec("python " + "/Users/mcoenca/Documents/Thnktwice/Code/sviep-bigdata-python/call_spca.py " + args_string, callback);
   };
 
   var wrappedPythonSPCA = Meteor.wrapAsync(executePythonSPCA);
 
   Meteor.methods({
-    executeSPCA: function(args) {
+    executeSPCA: function(args_dict) {
+      // args = []; args[0]=
+      // args_dict = {matrix_path: "/Users/mcoenca/Documents/Thnktwice/Code/data/many-results_matrix.csv", n_lines: "2950", n_col: "9000", word_path: "/Users/mcoenca/Documents/Thnktwice/Code/data/many-results_words.csv", k:"10", h:"8000", n_components: "2", norm_row: "True", precision: "1.0e-8"};
+      var args_string = dictToString(args_dict);
       if (!this.isSimulation){
         console.log("Starting spca...");
-        return wrappedPythonSPCA({}, supercalback);
+        return wrappedPythonSPCA(args_string, supercalback);
       }
     }
   });  
