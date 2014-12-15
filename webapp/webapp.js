4741cbb7c8f653c58ca6dadb6de7f62c8e51636e
mcoenca <m.coenca@gmail.com>
2014-12-15 19:08:47 +0100

    added arguments to the python call

--- webapp/webapp.js
+++ webapp/webapp.js
@@ -20,15 +20,18 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
   });
 
-    //Declare the methods on the server that can be accessed by the client
+  //Declare the methods on the server that can be accessed by the client
   var supercalback = function (error, stdout, stderr) {
         if (error) console.log(error);
-        if (stdout) console.log(stdout);
+        if (stdout) {
+          console.log(stdout);
+
+        }
         if (stderr) console.log(stderr);
     };
 
   var executePythonSPCA = function (args, callback) {
-    Meteor.npmRequire('child_process').exec("python " + "/Users/mcoenca/Documents/Thnktwice/Code/sviep-bigdata-python/main.py", callback);
+    Meteor.npmRequire('child_process').exec("python " + "/Users/mcoenca/Documents/Thnktwice/Code/sviep-bigdata-python/call_spca.py " + "/Users/mcoenca/Documents/Thnktwice/Code/data/many-results_matrix.csv 2950 9000 /Users/mcoenca/Documents/Thnktwice/Code/data/many-results_words.csv 10 8000 2 True 1.0e-8", callback);
   };
 
   var wrappedPythonSPCA = Meteor.wrapAsync(executePythonSPCA);
